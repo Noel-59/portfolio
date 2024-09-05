7a09d3e6c80973a32063413f9e81d343c2a3d757
Noel <noel.kivela1@edu.tampere.fi>
2024-09-05 14:27:49 +0300

    weeeweooo

---
+++ js/scroll.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var isScrolling = false;
+    var redirectUrl = "info.html"; 
+
+    function handleScroll() {
+        if (!isScrolling) {
+            isScrolling = true;
+
+            // Start the fade-out effect
+            document.body.classList.add('fade-out');
+
+            // Redirect after the fade-out transition
+            setTimeout(function() {
+                window.location.href = redirectUrl;
+            }, 1000); // Match this duration with the CSS transition time
+        }
+    }
+
+    // Attach the wheel event listener to the window object
+    window.addEventListener('wheel', handleScroll);
+});
